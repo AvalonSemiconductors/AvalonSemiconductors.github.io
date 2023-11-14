c2cbc3d03b33e7d7e4b741a48f3f5334b6d32e3a
Tholin <cutie@tholin.dev>
2023-11-14 18:36:46 +0100

    Add planned extended instructions.

--- AS2650/static/js/table.js
+++ AS2650/static/js/table.js
@@ -37,11 +37,12 @@ document.addEventListener('DOMContentLoaded', function(event) {
 		else if(CC == '5') CC_text = '<br>&emsp;&emsp;  00: r = V<br>&emsp;&emsp;  01: r > V<br>&emsp;&emsp;  10: r < V';
 		else if(CC == '3') CC_text = '<br>&emsp;&emsp;  00: All selected bits 1<br>&emsp;&emsp;  01: Not all selected bits 1<br>&emsp;&emsp;';
 		else if(CC == '4') CC_text = 'Loaded from r0';
+		else if(CC == '6') CC_text = "Loaded from stack";
 		var indir = val[2] == 'T';
 		var indexed = val[3] == 'T';
 		
 		$('.tooltip').remove();
-		$('<div class="tooltip"><b>Opcode:</b> ' + (prefix + '0123456789ABCDEF'.charAt($(this).parent().index() - 1) + '0123456789ABCDEF'.charAt($(this).index() - 1)) + '<br><b>Size (bytes):</b> ' + val[1] + '<br><b>C:</b> ' + (C == '+' || C == '1' ? 'Affected as defined' : 'Unchanged') + '<br><b>IDC:</b> ' + (IDC == '+' || IDC == '1' ? 'Affected as defined' : 'Unchanged') + '<br><b>OVF:</b> ' + (OVF == '+' || OVF == '1' ? 'Detects Overflow' : 'Unchanged') + '<br><b>CC:</b> ' + CC_text + '<br><b>Indirect:</b> ' + (indir ? 'Available' : 'Not Available') + '<br><b>Indexed:</b> ' + (indexed ? 'Available' : (val[3] == 'R' ? 'Required, r3 is index reg' : 'Not Available')) + (val[4] == 'T' ? '<br><b>Affected by WC flag.</b>' : (val[4] == 'C' ? '<br><b>Affected by COM flag.</b>' : '')) + '<br><br>' + val[5] + '<br>' + '</div>').css({'left': e.pageX + 10, 'top': e.pageY + 20}).appendTo('body > div');
+		$('<div class="tooltip"><b>Opcode:</b> ' + (prefix + '0123456789ABCDEF'.charAt($(this).parent().index() - 1) + '0123456789ABCDEF'.charAt($(this).index() - 1)) + '<br><b>Size (bytes):</b> ' + val[1] + '<br><b>C:</b> ' + (C == '+' || C == '1' ? 'Affected as defined' : 'Unchanged') + '<br><b>IDC:</b> ' + (IDC == '+' || IDC == '1' ? 'Affected as defined' : 'Unchanged') + '<br><b>OVF:</b> ' + (OVF == '+' || OVF == '1' ? 'Detects Overflow' : 'Unchanged') + '<br><b>CC:</b> ' + CC_text + '<br><b>Indirect:</b> ' + (indir ? 'Available' : 'Not Available') + '<br><b>Indexed:</b> ' + (indexed ? 'Available' : (val[3] == 'R' ? 'Required, r3 is index reg' : (val[3] == 'G' ? 'Required' : 'Not Available'))) + (val[4] == 'T' ? '<br><b>Affected by WC flag.</b>' : (val[4] == 'C' ? '<br><b>Affected by COM flag.</b>' : '')) + '<br><br>' + val[5] + '<br>' + '</div>').css({'left': e.pageX + 10, 'top': e.pageY + 20}).appendTo('body > div');
 	}).mousemove(function(e) {
 		$('.tooltip').css({'left': e.pageX + 10, 'top': e.pageY + 20});
 	});
@@ -82,7 +83,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
 				if(vals[2] == 'T' && vals[3] == 'T') {
 					$(t).addClass('highlight-yellow');
 				}
-				if(vals[2] == 'T' && vals[3] == 'R') {
+				if(vals[3] == 'R' || vals[3] == 'G') {
 					$(t).addClass('highlight-red');
 				}
 			});
